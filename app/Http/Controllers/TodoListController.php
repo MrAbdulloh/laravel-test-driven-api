57f4968bb27d3a9999554b12e3133fd13e3b7564
Abdulloh <akramovabdullo2002@gmail.com>
2023-01-25 22:09:53 +0300

    controller

---
+++ app/Http/Controllers/TodoListController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TodoList;
+use Illuminate\Http\Request;
+
+class TodoListController extends Controller
+{
+    public function index()
+    {
+        $lists = TodoList::all();
+
+        return response($lists);
+
+    }
+
+    public function show($id)
+    {
+        return response([]);
+    }
+}
