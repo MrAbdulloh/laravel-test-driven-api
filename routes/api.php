9450fa032cc7e608b168975a0663c40d4aa6c1f0
Abdulloh <akramovabdullo2002@gmail.com>
2023-01-27 02:48:55 +0300

    apiResponse create

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,18 @@
 <?php
 
 
+use App\Http\Controllers\TaskController;
 use App\Http\Controllers\TodoListController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::apiResource('todo-list', TodoListController::class);
-//
+
+Route::apiResource('todo-list.task', TaskController::class)
+    ->except('show')
+    ->shallow();
+
+//      route todo-list
 //Route::get('todo-list', [TodoListController::class, 'index'])->name('todo-list.index');
 //
 //Route::get('todo-list/{todolist}', [TodoListController::class, 'show'])->name('todo-list.show');
@@ -16,3 +22,11 @@ Route::apiResource('todo-list', TodoListController::class);
 //Route::delete('todo-list/{list}', [TodoListController::class, 'destroy'])->name('todo-list.destroy');
 //
 //Route::patch('todo-list/{list}', [TodoListController::class, 'update'])->name('todo-list.update');
+
+
+//      route task
+//Route::get('task', [TaskController::class, 'index'])->name('task.index');
+//
+//Route::post('task', [TaskController::class, 'store'])->name('task.store');
+//
+//Route::delete('task/{task}', [TaskController::class, 'destroy'])->name('task.destroy');
