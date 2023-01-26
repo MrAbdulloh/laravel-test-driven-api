eb715356c323df4ad9a7fd59b13531d2c898e88b
Abdulloh <akramovabdullo2002@gmail.com>
2023-01-27 00:05:01 +0300

    Api testing

--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -5,18 +5,49 @@ namespace App\Http\Controllers;
 use App\Models\TodoList;
 use Illuminate\Http\Request;
 
+use Symfony\Component\HttpFoundation\Response;
+
 class TodoListController extends Controller
 {
     public function index()
     {
-        $lists = TodoList::all();
+        $todo_list = TodoList::all();
 
-        return response($lists);
+        return response($todo_list);
 
     }
 
-    public function show($id)
+    public function show(TodoList $todo_list)
     {
-        return response([]);
+    return response($todo_list);
+//        dd($res);
     }
+
+    public function store(Request $request)
+    {
+//         validate
+        $request->validate(['name' =>[ 'required']]);
+
+        $lists = TodoList::query()->create($request->all());
+
+        return $lists;
+    }
+
+    public function destroy(TodoList $todo_list)
+    {
+        $todo_list->delete();
+
+        return response(' ', Response::HTTP_NO_CONTENT);
+    }
+
+    public function update(Request $request, TodoList $todo_list)
+    {
+        $request->validate(['name' => 'required']);
+
+$res=        $todo_list->update($request->all());
+//        dd($res);
+        return response($todo_list, Response::HTTP_UNPROCESSABLE_ENTITY);
+
+    }
+
 }
