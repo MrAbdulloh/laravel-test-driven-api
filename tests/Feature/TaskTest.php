10c0647aa3faaac8f3902ff7bcff649bf3e336ed
Abdulloh <akramovabdullo2002@gmail.com>
2023-01-27 11:14:59 +0300

    install TaskTest.php

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -2,66 +2,21 @@
 
 namespace Tests\Feature;
 
-use App\Models\Task;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TaskTest extends TestCase
 {
-    use RefreshDatabase;
-
-    /** @test */
-    public function fetch_all_tasks_of_a_todo_list()
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
     {
-        $list = $this->createTodoList();
+        $response = $this->get('/');
 
-        $task = $this->createTask();
-
-        $response = $this->getJson(route('todo-list.task.index', $list->id))->assertOk()->json();
-
-        $this->assertEquals(1, count($response));
-
-        $this->assertEquals($task->title, $response[0]['title']);
-
+        $response->assertStatus(200);
     }
-
-    /** @test */
-    public function store_a_task_for_a_todo_list()
-    {
-        $list = $this->createTodoList();
-        $task = $this->createTask();
-
-        $response = $this->postJson(route('todo-list.task.store', $list->id), ['title' => $task->title])
-            ->assertCreated();
-        $this->assertDatabaseHas('tasks', [
-            'title' => $task->title,
-            'todo_list_id' => $list->id,
-
-        ]);
-    }
-
-    /** @test */
-    public function delete_a_task_from_database()
-    {
-        $task = $this->createTask();
-
-        $this->deleteJson(route('task.destroy', $task->id))->assertNoContent();
-
-        $this->assertDatabaseMissing('tasks', ['title' => $task->title]);
-
-    }
-
-    /** @test */
-    public function update_a_task_of_a_todo_list()
-    {
-        $list = $this->createTodoList();
-        $task = $this->createTask();
-
-        $this->patchJson(route('task.update', $task->id), ['title' => 'update title'])
-        ->assertOk();
-
-        $this->assertDatabaseHas('tasks', ['title' => 'update title']);
-    }
-
 }
