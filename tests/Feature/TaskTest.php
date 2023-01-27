60372ee62e615386e44cc368b0bdd1a9aa8003b7
Abdulloh <akramovabdullo2002@gmail.com>
2023-01-27 14:41:43 +0300

    fetch all tasks of todo list

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -2,21 +2,26 @@
 
 namespace Tests\Feature;
 
+
+use App\Models\Task;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TaskTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /** @test */
+    public function fetch_all_tasks_of_a_todo_list()
+    {
+        $task = Task::factory()->create();
+
+        $response = $this->getJson(route('task.index'))->assertOk()->json();
+
+        $this->assertEquals(1, count($response));
+
+        $this->assertEquals($task->title, $response[0]['title']);
     }
 }
+
